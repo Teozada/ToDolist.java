bbb6246224b6bfb67299c59fd5b8e21b036a80a1
Teozada <teodoro.contato@hotmail.com>
2022-01-03 12:02:58 -0300

    subindo backend

---
+++ src/main/java/com/todolist/todolist/model/Task.java
@@ -0,0 +1,39 @@
+package com.todolist.todolist.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "tasks")
+@Setter
+@Getter
+@ToString
+
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Date deadLine;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/todolist/todolist/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.todolist.todolist.repository;
+
+import com.todolist.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository  extends JpaRepository<Task, Long> {
+
+}
